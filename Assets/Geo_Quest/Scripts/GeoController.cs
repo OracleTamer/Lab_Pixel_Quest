8fb1f811df17bd5016cc8f4a2da1eebeaf6d00d6
OracleTamer <jayreck1515@gmail.com>
2026-02-14 16:30:47 -0500

    visual studio practice

---
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoController : MonoBehaviour
+{
+    string variable2 = "World";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+        string variable1 = "Hello ";
+
+        Debug.Log(variable1 + variable2); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
