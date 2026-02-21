f7b5677dde3a9ed7c4304d83d0884efa6093e9e3
OracleTamer <jayreck1515@gmail.com>
2026-02-21 16:25:30 -0500

    Coding Added lesson 4

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GeoController : MonoBehaviour
 {
+    private int variable3 = 3;
     string variable2 = "World";
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,11 @@ public class GeoController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(variable3++);
+        transform.position += new Vector3(0.005f, 0, 0);
+       if (Input.GetKeyDown(KeyCode.W))
+       {
+            transform.position += new Vector3(0, 1, 0);
+       }
     }
 }
