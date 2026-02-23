f1877a793ca3a79f3cb37dc940b7c86a67e5dbd1
OracleTamer <jayreck1515@gmail.com>
2026-02-23 14:44:13 -0500

    Readded 2/21/26 scripts because I accidentally deleted everything like an idiot

---
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoController : MonoBehaviour
+{   
+    private int variable3 = 3;
+    string variable2 = "World";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+        string variable1 = "Hello ";
+
+        Debug.Log(variable1 + variable2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(variable3++);
+        transform.position += new Vector3(0.005f, 0, 0);
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+    }
+}
