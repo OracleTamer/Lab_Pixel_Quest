03b4ea5dc39ddfaf927df4206b63645a15aed398
OracleTamer <jayreck1515@gmail.com>
2026-03-10 17:57:37 -0400

    Began HW2 additions

---
+++ Assets/Hands-On Homework #2/Scripts/HW2 Right_Click_Bullet.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HW2Right_Click_Bullet : MonoBehaviour
+{
+    private Camera _camera;
+    private Vector3 _mousePos;
+    private Rigidbody2D _rigidbody2D;
+    public float speed = 5;
+    public float deathtime = 4f;
+    public bool playerBullet = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
